acd84bc29554ec61364e4c589fc8e92a10635abd
0xMatt <matthew.javelet@gmail.com>
2018-12-29 10:23:18 -0600

    Expand config components test coverage

--- packages/config/test/config.spec.ts
+++ packages/config/test/config.spec.ts
@@ -1,7 +1,24 @@
+import {Config} from './../src';
+
 describe('Config', () => {
 
-  it('can be loaded', () => {
+  const path = process.cwd() + '/packages/config/test/mocks';
 
+  it('can be loaded', () => {
+    const config = new Config(path);
+    expect(config.get()).toEqual({app: {foo: 'bar'}});
+  });
+
+  it('can get specific configurations', () => {
+    const config = new Config(path);
+    expect(config.get('app')).toEqual({foo: 'bar'});
+  })
+
+  it('can override existing settings', () => {
+    const config = new Config(path);
+    expect(config.get('app')).toEqual({foo: 'bar'});
+    config.set('app', {bar: 'baz'});
+    expect(config.get('app')).toEqual({bar: 'baz'});
   });
 
 });
